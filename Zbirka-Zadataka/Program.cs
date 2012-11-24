8271b1d6d390f87a1934e39680fadb1d163d5b2a
ikrstic <ikrstic@gmail.com>
2012-11-24 18:37:09 +0100

    Prvi commit

---
+++ Zbirka-Zadataka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbirka_Zadataka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
