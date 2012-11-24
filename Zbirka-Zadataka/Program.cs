f339798235a99150bca2517c59a116afeb3b4573
ikrstic <ikrstic@gmail.com>
2012-11-24 19:13:14 +0100

    izmenjena verzija prvog programa

--- Zbirka-Zadataka/Program.cs
+++ Zbirka-Zadataka/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Zbirka_Zadataka
 {
+    class Kupac
+    {
+        public double p1;
+        public string p2;
+        public int p3;
+
+        public Kupac(double p1, string p2, int p3)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+        /*
+        public double UkupnaKupovina {get; set;}
+        public string Ime {get; set;}
+        public int IdKupac {get; set;}*/
+
+        //Dodatne metode, događaji
+      /*  public Customer(double kupovina, string ime, int ID)
+            {
+                UkupnaKupovina = kupovina;
+                Ime = ime;
+                IdKupac = ID;
+            }
+            public string GetContactInfo() {return "ContactInfo";}
+            public string GetTransactionHistory() {return "History";}*/
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //Inicijalizacija novog objekta
+            Kupac cust1 = new Kupac(4987.63, "Pera Perić",90108);
+
+            //Izmena svojstva
+            //cust1.p1 += 499.99;
+            Console.WriteLine("Dodajte iznos na kupovinu");
+            cust1.p1 += Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ukupan iznos kupovne {0}",cust1.p1);
+            Console.ReadKey();
         }
     }
 }
