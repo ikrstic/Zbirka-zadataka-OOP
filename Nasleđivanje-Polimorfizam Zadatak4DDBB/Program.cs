09482feca94e503807372bbb5064e806f3b09e8a
ikrstic <ikrstic@gmail.com>
2012-11-25 14:12:54 +0100

    Zadatak 4

---
+++ Nasleđivanje-Polimorfizam Zadatak4DDBB/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasleđivanje_Polimorfizam_Zadatak4DDBB
+{
+    public class A
+    {
+        public virtual void Ispis()
+        { Console.WriteLine("A"); }
+    }
+
+    public class B : A
+    {
+        public override void Ispis()
+        { Console.WriteLine("B");}
+    }
+
+    public class C : B
+    {
+        new public virtual void Ispis()
+        { Console.WriteLine("C");}
+    }
+
+    public class D : C
+    {
+        public override void Ispis()
+        { Console.WriteLine("D");}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D d = new D();
+            C c = d;
+            B b = c;
+            A a = d;
+
+            d.Ispis();
+            c.Ispis();
+            b.Ispis();
+            a.Ispis();
+
+            Console.ReadKey();
+        }
+    }
+}
